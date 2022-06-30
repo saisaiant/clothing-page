5d18f316fcbdf7625da5f3737da21e2ac07fec61
kosai <saisaiant@gmail.com>
2022-06-30 22:09:23 +0630

    Add New css & scroll header change color in js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,11 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('scroll-header')
+                       : header.classList.remove('scroll-header')
+}
 
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SWIPER PRODUCTS ===============*/
 
