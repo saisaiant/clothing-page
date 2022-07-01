deb4b0854e90b35e86525bef226fabc629d93f5b
kosai <saisaiant@gmail.com>
2022-07-01 22:09:41 +0630

    Add scroll to top button

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,6 +48,15 @@ window.addEventListener('scroll', scrollActive)
 
 
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () => {
+  const scrollUp = document.getElementById('scroll-up')
+
+  this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                       : scrollUp.classList.remove('show-scroll') 
+}
+
+window.addEventListener('scroll', scrollUp)
+
 
 
 /*=============== DARK LIGHT THEME ===============*/ 
